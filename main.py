dd7255885244ae3a37dd73a879cbb3b7979e8f36
oliviafortuneUCD <oliviamfortune@gmail.com>
2022-04-25 23:09:41 +0100

    Test 1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+#print
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+    (x)
+
